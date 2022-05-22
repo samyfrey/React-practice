839193f7b2927004ea5d09287881fc1b76081e43
samyfrey <samyfrey@yahoo.com>
2022-05-22 12:22:58 -0400

    react jest testing practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css'
-// import Form from './components/Form'
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import ReactSwitch from 'react-switch'
+import { formatUserName } from './utils'
+import axios from 'axios'
 
 export const ThemeContext = createContext(null)
 
 function App() {
 	const [theme, setTheme] = useState('dark')
+ 	const [users, setUsers] = useState([])
 
+	 useEffect(() => {
+		let mounted = true;
+
+		const getUsers = async () => {
+			const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+			if (mounted) {
+			setUsers(response.data);
+			}
+		}
+		getUsers()
+	},[])
 	const toggleTheme = () => {
 		setTheme((curr) => (curr === 'light' ? 'dark' : 'light'))
 	}
@@ -19,6 +32,21 @@ function App() {
 					<label> {theme === 'light' ? 'Light Mode' : 'Dark Mode'}</label>
 					<ReactSwitch onChange={toggleTheme} checked={theme === 'dark'} />
 				</div>
+
+				<div>Users:</div>
+				{users.length ? (
+					<ul data-testid='user-list'>
+						{users.map((user) => (
+							<li key={user.id} className='user' data-testid='user-item'>
+								<span>{user.name}</span> (
+								<span>{formatUserName(user.username)}</span>)
+							</li>
+						))}
+					</ul>
+				) : (
+					<div>Loading users...</div>
+				)}
+
 			</div>
 		</ThemeContext.Provider>
 	)
